813244758adb9657cf5d12c60f38b05ed6c0e329
Dmitry Pavlov <dmitry.pavlov@toptal.com>
2019-01-07 22:22:19 +0300

    #7: Generate NSwag config file

--- src/OpenApiConnectedService.Package/Handler.cs
+++ src/OpenApiConnectedService.Package/Handler.cs
@@ -1,7 +1,12 @@
 ﻿using System;
+using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.ConnectedServices;
+using NSwag.Commands;
+using NSwag.Commands.CodeGeneration;
+using NSwag.Commands.SwaggerGeneration;
+using OpenApiConnectedService.Package.Utilities;
 
 namespace OpenApiConnectedService.Package
 {
@@ -25,6 +30,37 @@ namespace OpenApiConnectedService.Package
             var instance = (Instance) context.ServiceInstance;
 
             await context.Logger.WriteMessageAsync(LoggerMessageCategory.Information, $"Generating {instance.Name} from {instance.ServiceUri}");
+
+            var path = await GenerateNswagFileAsync(context, instance);
+
+            await context.Logger.WriteMessageAsync(LoggerMessageCategory.Information, $"Generated {path}");
+        }
+
+        private async Task<string> GenerateNswagFileAsync(ConnectedServiceHandlerContext context, Instance instance)
+        {
+            var nameSpace = context.ProjectHierarchy.GetProject().GetNameSpace();
+            var serviceFolder = instance.Name;
+            var serviceUrl = instance.ServiceUri;
+
+            var doc = NSwagDocument.Create();
+            doc.CodeGenerators.SwaggerToCSharpClientCommand = new SwaggerToCSharpClientCommand
+            {
+                OutputFilePath = $"{serviceFolder}Client.Generated.cs",
+                ClassName = $"{serviceFolder}Client",
+                Namespace = $"{nameSpace}.{serviceFolder}"
+            };
+            doc.SelectedSwaggerGenerator = new FromSwaggerCommand
+            {
+                OutputFilePath = $"{serviceFolder}.nswag.json",
+                Url = serviceUrl
+            };
+            var json = doc.ToJson();
+
+            var tempFileName = Path.GetTempFileName();
+            File.WriteAllText(tempFileName, json);
+
+            var targetPath = Path.Combine("Connected Services", serviceFolder, $"{serviceFolder}.nswag");
+            return await context.HandlerHelper.AddFileAsync(tempFileName, targetPath);
         }
     }
 }
---
+++ src/OpenApiConnectedService.Package/Utilities/ProjectHelper.cs
@@ -0,0 +1,27 @@
+﻿using EnvDTE;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.Shell.Interop;
+
+namespace OpenApiConnectedService.Package.Utilities
+{
+    /// <summary>
+    /// A utility class for working with Visual Studio project system.
+    /// </summary>
+    internal static class ProjectHelper
+    {
+        public static Project GetProject(this IVsHierarchy projectHierarchy)
+        {
+            int result = projectHierarchy.GetProperty(
+                VSConstants.VSITEMID_ROOT,
+                (int)__VSHPROPID.VSHPROPID_ExtObject,
+                out object projectObject);
+            ErrorHandler.ThrowOnFailure(result);
+            return (Project)projectObject;
+        }
+
+        public static string GetNameSpace(this Project project)
+        {
+            return project.Properties.Item("DefaultNamespace").Value.ToString();
+        }
+    }
+}
