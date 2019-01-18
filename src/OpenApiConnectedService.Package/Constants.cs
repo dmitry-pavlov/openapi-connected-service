ea320ccc7e5c99a83b0b29841794a9f7a6d38188
Dmitry Pavlov <dmitry.pavlov@toptal.com>
2019-01-18 20:40:00 +0300

    #17: Update graphics and branding

--- src/OpenApiConnectedService.Package/Constants.cs
+++ src/OpenApiConnectedService.Package/Constants.cs
@@ -4,7 +4,7 @@
     {
         public const string Author = "Dmitry Pavlov";
         public const string ExtensionCategory = "OpenAPI";
-        public const string ExtensionName = "OpenAPI Connected Service";
+        public const string ExtensionName = "OpenAPI (Swagger) Connected Service";
         public const string ExtensionDescription = "Generates API client proxy code for OpenAPI (Swagger) web service with NSwag.";
         public const string ProviderId = "DmitryPavlov.OpenApiConnectedService";
         public const string Website = "https://github.com/dmitry-pavlov/openapi-connected-service/blob/master/README.MD";
