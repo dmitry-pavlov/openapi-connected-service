a3d57a87da558765b99d53faa82933b2dc9dca94
Dmitry Pavlov <dmitry.pavlov@toptal.com>
2019-02-03 16:42:33 +0300

    #22: added Samples.AspNetCoreMvc.ClientInjectedToStartup

---
+++ docs/samples/Samples.AspNetCoreMvc.ClientInjectedToStartup/Connected Services/PetStore/PetStoreClient.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Samples.AspNetCoreMvc.ClientInjectedToStartup.PetStore
+{
+    /// <summary>This partial class allows to wrap and extend the generated client logic if you need that.</summary>
+    public partial class Client : IPetStoreClient
+    {
+        // Note: lets add interface methods we want to expose in our appliction via dependency 
+
+        public async Task<int> GetSoldPetsCount()
+        {
+            ICollection<Pet> soldPets = await this.FindPetsByStatusAsync(new[] { Anonymous.Sold });
+            return soldPets.Count;
+        }
+    }
+
+    /// <summary>Interface defines what we want to expose in our appliction via dependency injection.</summary>
+    public interface IPetStoreClient
+    {
+        /// <summary>Returns number of all sold pets in the store.</summary>
+        Task<int> GetSoldPetsCount();
+
+        // NOTE: you can also define here methods from generated Client partial class to expose them from interface.
+    }
+}
---
+++ docs/samples/Samples.AspNetCoreMvc.ClientInjectedToStartup/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Samples.AspNetCoreMvc.ClientInjectedToStartup.Models;
+using Samples.AspNetCoreMvc.ClientInjectedToStartup.PetStore;
+
+namespace Samples.AspNetCoreMvc.ClientInjectedToStartup.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IPetStoreClient _petStoreClient;
+
+        public HomeController(IPetStoreClient petStoreClient)
+        {
+            _petStoreClient = petStoreClient;
+        }
+
+        public IActionResult Index()
+        {
+            TempData["SoldPetsCount"] = _petStoreClient.GetSoldPetsCount().Result;
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
